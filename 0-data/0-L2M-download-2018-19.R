8cca5ba5582834377052fcafd4cf8230a85204f4
robert <atlhawksfanatic@gmail.com>
2021-03-28 21:45:53 -0400

    changing default to RSelenium

--- 0-data/0-L2M-download-2018-19.R
+++ 0-data/0-L2M-download-2018-19.R
@@ -4,15 +4,15 @@
 
 # ---- start --------------------------------------------------------------
 
-library("httr")
-library("rvest")
-library("tidyverse")
-library("splashr")
+library(httr)
+library(RSelenium)
+library(rvest)
+library(tidyverse)
 
 # Create a directory for the data
 local_dir     <- "0-data/L2M/2018-19"
 data_source   <- paste0(local_dir, "/raw")
-scrape_source <- paste0(data_source, "/scraped_splashr")
+scrape_source <- paste0(data_source, "/scraped_rselenium")
 if (!file.exists(local_dir)) dir.create(local_dir, recursive = T)
 if (!file.exists(data_source)) dir.create(data_source, recursive = T)
 if (!file.exists(scrape_source)) dir.create(scrape_source, recursive = T)
@@ -71,12 +71,32 @@ links_url    <- links_url[!(gsub(".*\\?|&.*", "", links_url) %in%
 # links_url    <- links_url[!(gsub(".*\\?|&.*", "", links_url) %in%
 #                               good_data$game_id)]
 
-# ---- splashr-start -------------------------------------------------------
+# ---- RSelenium-start ----------------------------------------------------
 
-# system("sudo docker run -it -p 8050:8050 scrapinghub/splash")
-# system("docker run -p 8050:8050 scrapinghub/splash")
+# # Uncomment if you do not have the container running automatically on startup
+# system("docker run -d -p 4445:4444 selenium/standalone-firefox")
+# Sys.sleep(7) # Give Docker some time to load up
 
-splash_active() # This needs to be TRUE to work...
+rem_dr <- remoteDriver(remoteServerAddr = "localhost",
+                       port = 4445L,
+                       browserName = "firefox")
+rem_dr$open(silent = T)
+
+# Wait until the page actually loads:
+wetest <- function(sleepmin = 0.1, sleepmax = 0.3){
+  remDr       <- get("rem_dr", envir = globalenv())
+  webElemtest <- NULL
+  
+  # Keep checking until the error does not happen
+  while(is.null(webElemtest)){
+    webElemtest <- tryCatch({
+      # One portion of the l2m report that is always present is team logos:
+      suppressMessages(rem_dr$findElement(using = "class name", "nbalogo"))
+    }, error = function(e){NULL})
+  }
+  randsleep <- sample(seq(sleepmin, sleepmax, by = 0.001), 1)
+  Sys.sleep(randsleep)
+}
 
 # ---- map-links ----------------------------------------------------------
 
@@ -84,15 +104,19 @@ scrape_site <- map(links_url, function(x) {
   game_id <- gsub(".*\\?|&.*", "", x)
   print(paste0(game_id, " at ", Sys.time()))
   
-  Sys.sleep(runif(1, 10, 15))
+  Sys.sleep(runif(1, 1, 3))
   
-  l2m_raw <- render_html(url = x, wait = 7)
+  rem_dr$navigate(x)
+  
+  wetest()
+  
+  l2m_raw <- read_html(rem_dr$getPageSource()[[1]])
   
   game_details <- l2m_raw %>% 
-    xml_nodes(".gamedetails") %>% 
+    html_elements(".gamedetails") %>% 
     html_text()
   game_date <- l2m_raw %>% 
-    xml_nodes(".gamedate") %>% 
+    html_elements(".gamedate") %>% 
     html_text()
   
   l2m_site <- l2m_raw  %>% 
@@ -120,15 +144,15 @@ scrape_site <- map(links_url, function(x) {
              time = str_remove(time, "Time:"),
              call_type = str_remove(call_type, "Call Type:"),
              committing = str_remove(committing_player,
-                                              "Committing Player:"),
+                                     "Committing Player:"),
              disadvantaged = str_remove(disadvantaged_player,
-                                                 "Disadvantaged Player:"),
+                                        "Disadvantaged Player:"),
              decision = str_remove(review_decision,
-                                            "Review Decision:")) %>% 
+                                   "Review Decision:")) %>% 
       mutate_all(str_trim) %>% 
       select(period, time, call_type, committing, disadvantaged, decision,
              comments, stint)
-      
+    
     
     # Check to see that these are dataframes with more than 1 row
     n1 = nrow(data2)
@@ -143,7 +167,7 @@ scrape_site <- map(links_url, function(x) {
       return(j5)
     } else if (n1 == 0) {
       print("didn't work, data were 0 length")
-      j5 <- data.frame(Period = "error - n1 == 0",
+      j5 <- data.frame(period = "error - n1 == 0",
                        game_id = game_id,
                        game_details = game_details,
                        game_date = game_date,
@@ -152,7 +176,7 @@ scrape_site <- map(links_url, function(x) {
       return(j5)
     } else {
       print("didn't work, not sure")
-      j5 <- data.frame(Period = "error - huh",
+      j5 <- data.frame(period = "error - huh",
                        game_id = game_id,
                        game_details = game_details,
                        game_date = game_date,
@@ -164,7 +188,7 @@ scrape_site <- map(links_url, function(x) {
     
   } else {
     print("no")
-    j5 <- data.frame(Period = "error",
+    j5 <- data.frame(period = "error",
                      game_id = game_id,
                      game_details = game_details,
                      game_date = game_date,
@@ -175,14 +199,22 @@ scrape_site <- map(links_url, function(x) {
   
 })
 
+
+# ---- extra --------------------------------------------------------------
+
+
 # Now, write the full data set and also write individual csv files!
 if (is_empty(scrape_site)) {
   scrape_data <- data.frame(scrape_time = NA)
   
 } else {
   scrape_data  <- bind_rows(scrape_site) %>% 
+    # Filter out the errors
+    filter(!grepl("error", period)) %>% 
     mutate(scrape_time = as.character(scrape_time))
   
+  # Just in case all were errors.
+  if (nrow(scrape_data) == 0) scrape_data <- data.frame(scrape_time = NA)
 }
 
 scraped_data <- map(scraped_files, read_csv, col_types = cols(.default = "c"))
@@ -190,6 +222,8 @@ scraped_data <- map(scraped_files, read_csv, col_types = cols(.default = "c"))
 # Individual games
 ind_games_csv <- map(scrape_site, function(x) {
   game_id <- x$game_id[1]
+  
+  # If the data.frame in the list only has one observation it's an error
   if (nrow(x) > 1) {
     write_csv(x, paste0(scrape_source, "/", game_id, ".csv"))
     return(data.frame(game_id, status = "good"))
@@ -198,8 +232,8 @@ ind_games_csv <- map(scrape_site, function(x) {
   }
 })
 
-corrections <- scrape_data %>% 
-  bind_rows(scraped_data) %>%
+corrections <- scraped_data %>% 
+  bind_rows(scrape_data) %>%
   arrange(scrape_time) %>% 
   filter(!is.na(scrape_time))
 
@@ -211,10 +245,14 @@ corrections <- corrections %>%
          home1 = str_trim(str_remove(game_details, ".*@")),
          home_score = str_extract(home1,  "(?<=\\().+?(?=\\))"),
          home_team = str_trim(str_remove(home1, "\\(.*\\)"))) %>% 
-  select(-away1, -home1)
+  select(-away1, -home1) %>% 
+  # Put the corrections into a consistent order
+  arrange(game_id, period, desc(time))
 
+write_csv(corrections, paste0(local_dir, "/scraped_201819_rselenium.csv"))
+write_rds(corrections, paste0(local_dir, "/scraped_201819_rselenium.rds"))
 
-write_csv(corrections, paste0(local_dir, "/scraped_201819.csv"))
-write_rds(corrections, paste0(local_dir, "/scraped_201819.rds"))
-
-
+# Get rid of the RSelenium instances
+rem_dr$close()
+rm(rem_dr)
+gc()
--- 0-data/0-L2M-download-2020-21.R
+++ 0-data/0-L2M-download-2020-21.R
@@ -4,15 +4,15 @@
 
 # ---- start --------------------------------------------------------------
 
-library("httr")
-library("rvest")
-library("tidyverse")
-library("splashr")
+library(httr)
+library(RSelenium)
+library(rvest)
+library(tidyverse)
 
 # Create a directory for the data
 local_dir     <- "0-data/L2M/2020-21"
 data_source   <- paste0(local_dir, "/raw")
-scrape_source <- paste0(data_source, "/scraped_splashr")
+scrape_source <- paste0(data_source, "/scraped_rselenium")
 if (!file.exists(local_dir)) dir.create(local_dir, recursive = T)
 if (!file.exists(data_source)) dir.create(data_source, recursive = T)
 if (!file.exists(scrape_source)) dir.create(scrape_source, recursive = T)
@@ -68,105 +68,113 @@ links_url <- links_url[!(gsub(".*\\?|&.*", "", links_url) %in%
 # links_url    <- links_url[!(gsub(".*\\?|&.*", "", links_url) %in%
 #                               good_data$game_id)]
 
-# ---- splashr-start -------------------------------------------------------
 
-# Only start splashr if there are urls to be downloaded.
+# ---- RSelenium-start ----------------------------------------------------
 
-if (!is_empty(links_url)) {
-  # Create docker container for splashr to scrape
-  splash_container <- start_splash(container_name = "l2m")
-  Sys.sleep(3)
+# # Uncomment if you do not have the container running automatically on startup
+# system("docker run -d -p 4445:4444 selenium/standalone-firefox")
+# Sys.sleep(7) # Give Docker some time to load up
+
+rem_dr <- remoteDriver(remoteServerAddr = "localhost",
+                       port = 4445L,
+                       browserName = "firefox")
+rem_dr$open(silent = T)
+
+# Wait until the page actually loads:
+wetest <- function(sleepmin = 0.1, sleepmax = 0.3){
+  remDr       <- get("rem_dr", envir = globalenv())
+  webElemtest <- NULL
+  
+  # Keep checking until the error does not happen
+  while(is.null(webElemtest)){
+    webElemtest <- tryCatch({
+      # One portion of the l2m report that is always present is team logos:
+      suppressMessages(rem_dr$findElement(using = "class name", "nbalogo"))
+    }, error = function(e){NULL})
+  }
+  randsleep <- sample(seq(sleepmin, sleepmax, by = 0.001), 1)
+  Sys.sleep(randsleep)
 }
-
 
 # ---- map-links ----------------------------------------------------------
 
-if (splash_active()) {
-  scrape_site <- map(links_url, function(x) {
-    game_id <- gsub(".*\\?|&.*", "", x)
-    print(paste0(game_id, " at ", Sys.time()))
+scrape_site <- map(links_url, function(x) {
+  game_id <- gsub(".*\\?|&.*", "", x)
+  print(paste0(game_id, " at ", Sys.time()))
+  
+  Sys.sleep(runif(1, 1, 3))
+  
+  rem_dr$navigate(x)
+  
+  wetest()
+  
+  l2m_raw <- read_html(rem_dr$getPageSource()[[1]])
+  
+  game_details <- l2m_raw %>% 
+    html_elements(".gamedetails") %>% 
+    html_text()
+  game_date <- l2m_raw %>% 
+    html_elements(".gamedate") %>% 
+    html_text()
+  
+  l2m_site <- l2m_raw  %>% 
+    html_table(fill = T) %>% 
+    .[[1]]
+  
+  # Did the scrape give us a functional table?
+  
+  if (is.data.frame(l2m_site)) {
+    print("is a data.frame")
+    l2m_site <- l2m_site[, !is.na(names(l2m_site)) & !(names(l2m_site) == "")]
     
-    Sys.sleep(runif(1, 10, 15))
+    names(l2m_site) <- tolower(str_replace(names(l2m_site), " ", "_"))
     
-    l2m_raw <- render_html(url = x, wait = 7)
+    data1 <- l2m_site %>% 
+      mutate(comments = if_else(grepl(pattern = "^Comment", period),
+                                time, NA_character_),
+             comments = lead(comments),
+             stint = if_else(period == "", time, NA_character_)) %>% 
+      fill(stint)
     
-    game_details <- l2m_raw %>% 
-      xml_nodes(".gamedetails") %>% 
-      html_text()
-    game_date <- l2m_raw %>% 
-      xml_nodes(".gamedate") %>% 
-      html_text()
+    data2 <- data1 %>% 
+      filter(grepl(pattern = "^Period", period)) %>% 
+      mutate(period = str_remove(period, "Period:"),
+             time = str_remove(time, "Time:"),
+             call_type = str_remove(call_type, "Call Type:"),
+             committing = str_remove(committing_player,
+                                     "Committing Player:"),
+             disadvantaged = str_remove(disadvantaged_player,
+                                        "Disadvantaged Player:"),
+             decision = str_remove(review_decision,
+                                   "Review Decision:")) %>% 
+      mutate_all(str_trim) %>% 
+      select(period, time, call_type, committing, disadvantaged, decision,
+             comments, stint)
     
-    l2m_site <- l2m_raw  %>% 
-      html_table(fill = T) %>% 
-      .[[1]]
     
-    # Did the scrape give us a functional table?
-    
-    if (is.data.frame(l2m_site)) {
-      print("is a data.frame")
-      l2m_site <- l2m_site[, !is.na(names(l2m_site)) & !(names(l2m_site) == "")]
+    # Check to see that these are dataframes with more than 1 row
+    n1 = nrow(data2)
+    if (n1 > 0) {
+      print("it worked!")
+      j5 <- data2
+      j5$game_id      <- game_id
+      j5$game_details <- game_details
+      j5$game_date    <- game_date
+      j5$scrape_time  <- Sys.time()
       
-      names(l2m_site) <- tolower(str_replace(names(l2m_site), " ", "_"))
+      return(j5)
+    } else if (n1 == 0) {
+      print("didn't work, data were 0 length")
+      j5 <- data.frame(period = "error - n1 == 0",
+                       game_id = game_id,
+                       game_details = game_details,
+                       game_date = game_date,
+                       scrape_time = Sys.time())
       
-      data1 <- l2m_site %>% 
-        mutate(comments = if_else(grepl(pattern = "^Comment", period),
-                                  time, NA_character_),
-               comments = lead(comments),
-               stint = if_else(period == "", time, NA_character_)) %>% 
-        fill(stint)
-      
-      data2 <- data1 %>% 
-        filter(grepl(pattern = "^Period", period)) %>% 
-        mutate(period = str_remove(period, "Period:"),
-               time = str_remove(time, "Time:"),
-               call_type = str_remove(call_type, "Call Type:"),
-               committing = str_remove(committing_player,
-                                       "Committing Player:"),
-               disadvantaged = str_remove(disadvantaged_player,
-                                          "Disadvantaged Player:"),
-               decision = str_remove(review_decision,
-                                     "Review Decision:")) %>% 
-        mutate_all(str_trim) %>% 
-        select(period, time, call_type, committing, disadvantaged, decision,
-               comments, stint)
-      
-      
-      # Check to see that these are dataframes with more than 1 row
-      n1 = nrow(data2)
-      if (n1 > 0) {
-        print("it worked!")
-        j5 <- data2
-        j5$game_id      <- game_id
-        j5$game_details <- game_details
-        j5$game_date    <- game_date
-        j5$scrape_time  <- Sys.time()
-        
-        return(j5)
-      } else if (n1 == 0) {
-        print("didn't work, data were 0 length")
-        j5 <- data.frame(period = "error - n1 == 0",
-                         game_id = game_id,
-                         game_details = game_details,
-                         game_date = game_date,
-                         scrape_time = Sys.time())
-        
-        return(j5)
-      } else {
-        print("didn't work, not sure")
-        j5 <- data.frame(period = "error - huh",
-                         game_id = game_id,
-                         game_details = game_details,
-                         game_date = game_date,
-                         scrape_time = Sys.time())
-        
-        return(j5)
-      }
-      
-      
+      return(j5)
     } else {
-      print("no")
-      j5 <- data.frame(period = "error",
+      print("didn't work, not sure")
+      j5 <- data.frame(period = "error - huh",
                        game_id = game_id,
                        game_details = game_details,
                        game_date = game_date,
@@ -175,18 +183,23 @@ if (splash_active()) {
       return(j5)
     }
     
-  })
+    
+  } else {
+    print("no")
+    j5 <- data.frame(period = "error",
+                     game_id = game_id,
+                     game_details = game_details,
+                     game_date = game_date,
+                     scrape_time = Sys.time())
+    
+    return(j5)
+  }
   
-  
-} else {
-  print("Splash is not running, please enable before scraping.")
-  scrape_site <- list(data.frame(period = "error - splashr did not start",
-                                 game_id = NA, game_details = NA,
-                                 game_date = NA, scrape_time = Sys.time()))
-}
+})
 
-# Remove the splash_container if it was created
-if (exists("splash_container")) stop_splash(splash_container)
+
+# ---- extra --------------------------------------------------------------
+
 
 # Now, write the full data set and also write individual csv files!
 if (is_empty(scrape_site)) {
@@ -234,5 +247,10 @@ corrections <- corrections %>%
   # Put the corrections into a consistent order
   arrange(game_id, period, desc(time))
 
-write_csv(corrections, paste0(local_dir, "/scraped_202021.csv"))
-write_rds(corrections, paste0(local_dir, "/scraped_202021.rds"))
+write_csv(corrections, paste0(local_dir, "/scraped_202021_rselenium.csv"))
+write_rds(corrections, paste0(local_dir, "/scraped_202021_rselenium.rds"))
+
+# Get rid of the RSelenium instances
+rem_dr$close()
+rm(rem_dr)
+gc()
