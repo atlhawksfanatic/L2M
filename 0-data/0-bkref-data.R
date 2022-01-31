6cb4e5fbc20c1d3e4864c51061f8b24d499bc184
robert <atlhawksfanatic@gmail.com>
2022-01-31 08:34:44 -0500

    reorder readme files

--- 0-data/0-bkref-data.R
+++ 0-data/0-bkref-data.R
@@ -12,166 +12,9 @@ data_source <- paste0(local_dir, "/raw")
 if (!file.exists(local_dir)) dir.create(local_dir, recursive = T)
 if (!file.exists(data_source)) dir.create(data_source, recursive = T)
 
-
-# ---- team-list ----------------------------------------------------------
-
-# Only download the bkref team id list once and save it
-
-if (file.exists(paste0(local_dir, "/team_ids.rds"))) {
-  team_list <- read_rds(paste0(local_dir, "/team_ids.rds"))
-} else {
-  team_list <- paste0("https://raw.githubusercontent.com/bttmly/",
-                      "nba/master/data/teams.json") %>% 
-    GET() %>% 
-    httr::content(as = "text", encoding = "UTF-8") %>% 
-    jsonlite::fromJSON() %>% 
-    mutate_all(as.character)
-  
-  write_csv(team_list, paste0(local_dir, "/team_ids.csv"))
-  write_rds(team_list, paste0(local_dir, "/team_ids.rds"))
-  
-}
-
-team_dictionary <- team_list$abbreviation
-names(team_dictionary) <- team_list$simpleName
-
-
 # ---- game-calls ---------------------------------------------------------
 
-# Archived
-archived <- read_rds("0-data/L2M/archive/pdftools_L2M_archive_all.rds") %>% 
-  mutate(call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         date = mdy(game_date),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2017-18
-l2m_2018 <- read_rds("0-data/L2M/2017-18/pdftools_L2M_201718.rds") %>% 
-  mutate(date = mdy(game_date),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2018-19 pdfs
-l2mpdf_2019 <- read_rds("0-data/L2M/2018-19/pdftools_L2M_201819_all.rds") %>% 
-  mutate(date = mdy(game_date),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2018-19 scraped
-scraped_2019 <- read_rds("0-data/L2M/2018-19/scraped_201819.rds") %>% 
-  select(-game_id, -away_score, -home_score) %>% 
-  mutate(date = mdy(game_date),
-         decision = case_when(decision == "NCC" ~ "CNC",
-                              decision == "NCI" ~ "INC",
-                              decision == "Undetectable" ~ "",
-                              T ~ decision),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2019-20 pdfs
-l2mpdf_2020 <- read_rds("0-data/L2M/2019-20/pdftools_L2M_201920_all.rds") %>% 
-  mutate(date = mdy(game_date),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2019-20 scraped
-scraped_2020 <- read_rds("0-data/L2M/2019-20/scraped_201920.rds") %>% 
-  select(-game_id, -away_score, -home_score) %>% 
-  mutate(date = mdy(game_date),
-         decision = case_when(decision == "NCC" ~ "CNC",
-                              decision == "NCI" ~ "INC",
-                              decision == "Undetectable" ~ "",
-                              T ~ decision),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2020-21 scraped
-scraped_2021 <- read_rds("0-data/L2M/2020-21/scraped_202021.rds") %>% 
-  select(-game_id, -away_score, -home_score) %>% 
-  mutate(date = mdy(game_date),
-         decision = case_when(decision == "NCC" ~ "CNC",
-                              decision == "NCI" ~ "INC",
-                              decision == "Undetectable" ~ "",
-                              T ~ decision),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# 2021-22 scraped
-scraped_2022 <- read_rds("0-data/L2M/2021-22/scraped_202122.rds") %>% 
-  select(-game_id, -away_score, -home_score) %>% 
-  mutate(date = mdy(game_date),
-         decision = case_when(decision == "NCC" ~ "CNC",
-                              decision == "NCI" ~ "INC",
-                              decision == "Undetectable" ~ "",
-                              T ~ decision),
-         call_type = ifelse(call_type == "N/A" | call_type == "Other",
-                            NA_character_, call_type),
-         call_type = str_squish(call_type),
-         call = str_remove(call_type, ":.*"),
-         type = str_trim(str_remove(call_type, ".*:")),
-         home = team_dictionary[home_team],
-         away = team_dictionary[away_team])
-
-# L2M are not consistent with the names of teams
-team_cross <- c("BRK" = "BKN", "PHO" = "PHX")
-
-bkref_cross <- c("BKN" = "BRK", "PHX" = "PHO", "CHA" = "CHO")
-
-l2m_games <- archived %>% 
-  bind_rows(l2m_2018) %>% 
-  bind_rows(l2mpdf_2019) %>% 
-  bind_rows(scraped_2019) %>% 
-  bind_rows(l2mpdf_2020) %>% 
-  bind_rows(scraped_2020) %>% 
-  bind_rows(scraped_2021) %>% 
-  bind_rows(scraped_2022) %>% 
-  select(-away_score, -home_score) %>% 
-  mutate(home = ifelse(is.na(team_cross[home]),
-                       home, team_cross[home]),
-         away = ifelse(is.na(team_cross[away]),
-                       away, team_cross[away]),
-         
-         home_team = ifelse(is.na(team_dictionary[home]),
-                            home, team_dictionary[home]),
-         away_team = ifelse(is.na(team_dictionary[away]),
-                            away, team_dictionary[away]),
-         home_bkref = ifelse(is.na(bkref_cross[home_team]),
-                             home_team, bkref_cross[home_team]),
-         
-         bkref_id = paste0(str_remove_all(date, "-"), "0",
-                           home_bkref))
+l2m_games <- read_csv("1-tidy/L2M/L2M_raw.csv")
 
 # ---- game-ids -----------------------------------------------------------
 
@@ -184,8 +27,8 @@ bkref_games <- l2m_games %>%
 # Been getting an error about HTTP2 framing layer
 httr::set_config(httr::config(http_version = 0))
 
-if (file.exists("0-data/bkref/bkref_box.rds")) {
-  old_box <- read_rds("0-data/bkref/bkref_box.rds")
+if (file.exists("0-data/bkref/bkref_box.csv")) {
+  old_box <- read_csv("0-data/bkref/bkref_box.csv")
 } else {
   old_box <- data.frame(bkref_id = NA)
 }
